9f1c8f58840428e35957539d3b6f9545066dfe79
fiarcherry <fiarchersschannel@gmail.com>
2021-03-30 19:34:20 +0300

    add isDisabled=false prop

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,5 +1,7 @@
-const Button = ({ onClick, title }) => (
-  <button onClick={onClick}>{title}</button>
+const Button = ({ onClick, title, isDisabled = false }) => (
+  <button onClick={onClick} disabled={isDisabled}>
+    {title}
+  </button>
 )
 
 export default Button

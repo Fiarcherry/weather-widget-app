c5af56911243710a7eaa242f47b3e67ae21c40af
fiarcherry <fiarchersschannel@gmail.com>
2021-03-31 18:16:37 +0300

    Store using reduxjs/toolkit

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,19 +1,16 @@
-import { applyMiddleware, createStore } from 'redux'
-import thunkMiddleware from 'redux-thunk'
-import { composeWithDevTools } from 'redux-devtools-extension'
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit'
 
 import monitorReducersEnhancer from './enhancers/monitorReducer'
 import loggerMiddleware from './middleware/logger'
 import rootReducer from './reducers'
 
-export default function configureStore(preloadedState) {
-  const middlewares = [loggerMiddleware, thunkMiddleware]
-  const middlewareEnhancer = applyMiddleware(...middlewares)
-
-  const enhancers = [middlewareEnhancer, monitorReducersEnhancer]
-  const composedEnhancers = composeWithDevTools(...enhancers)
-
-  const store = createStore(rootReducer, preloadedState, composedEnhancers)
+export default function configureAppStore(preloadedState) {
+  const store = configureStore({
+    reducer: rootReducer,
+    middleware: [loggerMiddleware, ...getDefaultMiddleware()],
+    preloadedState,
+    enhancers: [monitorReducersEnhancer]
+  })
 
   if (process.env.NODE_ENV !== 'production' && module.hot) {
     module.hot.accept('./reducers', () => store.replaceReducer(rootReducer))

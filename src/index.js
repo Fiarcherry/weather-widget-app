1d51680705682c3c8656d2375b44f5f374657fb8
fiarcherry <fiarchersschannel@gmail.com>
2021-03-31 18:10:31 +0300

    Configure store

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
-import store from './redux/store'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { render } from 'react-dom'
 import { Provider } from 'react-redux'
+import configureStore from './redux/configureStore'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import App from './components/App'
+import './index.css'
 //import * as serviceWorker from './serviceWorker'
 
-ReactDOM.render(
+const store = configureStore()
+
+render(
   <React.StrictMode>
     <Provider store={store}>
       <Router>

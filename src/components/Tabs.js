21cafa43c789a098858150421ea5fd4ea2317941
fiarcherry <fiarchersschannel@gmail.com>
2021-03-31 17:52:34 +0300

    Add Links to Buttons

--- src/components/Tabs.js
+++ src/components/Tabs.js
@@ -3,25 +3,32 @@ import { connect } from 'react-redux'
 import { changeTab } from '../redux/actions'
 import { TAB_TYPES } from '../constants/tabs'
 import Button from './Button'
+import { Link } from 'react-router-dom'
 
 const Tabs = ({ activeTab, changeTab }) => (
   <div>
     <span>Show: </span>
-    <Button
-      onClick={() => changeTab(TAB_TYPES.ALL)}
-      title={TAB_TYPES.ALL}
-      isDisabled={activeTab === TAB_TYPES.ALL ? true : false}
-    />
-    <Button
-      onClick={() => changeTab(TAB_TYPES.ACTIVE)}
-      title={TAB_TYPES.ACTIVE}
-      isDisabled={activeTab === TAB_TYPES.ACTIVE ? true : false}
-    />
-    <Button
-      onClick={() => changeTab(TAB_TYPES.DELETED)}
-      title={TAB_TYPES.DELETED}
-      isDisabled={activeTab === TAB_TYPES.DELETED ? true : false}
-    />
+    <Link to="/all">
+      <Button
+        onClick={() => changeTab(TAB_TYPES.ALL)}
+        title={TAB_TYPES.ALL}
+        isDisabled={activeTab === TAB_TYPES.ALL ? true : false}
+      ></Button>
+    </Link>
+    <Link to="/active">
+      <Button
+        onClick={() => changeTab(TAB_TYPES.ACTIVE)}
+        title={TAB_TYPES.ACTIVE}
+        isDisabled={activeTab === TAB_TYPES.ACTIVE ? true : false}
+      ></Button>
+    </Link>
+    <Link to="/deleted">
+      <Button
+        onClick={() => changeTab(TAB_TYPES.DELETED)}
+        title={TAB_TYPES.DELETED}
+        isDisabled={activeTab === TAB_TYPES.DELETED ? true : false}
+      ></Button>
+    </Link>
   </div>
 )
 

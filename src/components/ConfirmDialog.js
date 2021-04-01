74fbaa98d9e35fff38abf0b6098137683a050616
fiarcherry <fiarchersschannel@gmail.com>
2021-04-01 18:04:22 +0300

    Update ConfirmDialog.js

--- src/components/ConfirmDialog.js
+++ src/components/ConfirmDialog.js
@@ -20,20 +20,23 @@ const ConfirmDialog = ({ row, rowToggle }) => {
 
   return (
     <div>
-      <Button onClick={handleClickOpen} variant="outlined" color="primary">
+      <Button
+        onClick={() => handleClickOpen}
+        variant="outlined"
+        color="primary"
+      >
         {!row.active ? 'Восстановить' : 'Удалить'}
       </Button>
       <Dialog
         open={open}
-        onClose={handleCancel}
+        onClose={() => handleCancel}
         aria-labelledby="alert-dialog-title"
-        aria-describedby="alert-dialog-description"
       >
         <DialogTitle id="alert-dialog-title">
           {`Удалить город ${row.name} из списка?`}
         </DialogTitle>
         <DialogActions>
-          <Button onClick={handleCancel} color="primary">
+          <Button onClick={() => handleCancel} color="primary">
             Отмена
           </Button>
           <Button onClick={() => handleOk(row.id)} color="primary" autoFocus>

4ae845e84477ba4a4e8fb26c90fb48ca4328214e
fiarcherry <fiarchersschannel@gmail.com>
2021-04-01 15:42:09 +0300

    Update selectors.js

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,13 +1,19 @@
 import { TAB_TYPES } from '../constants/tabs'
 
-export const getRowsByTab = (rows, tab) => {
-  switch (tab) {
+export const getRowsByTab = (state) => {
+  const { table, tabs } = state
+  switch (tabs) {
     case TAB_TYPES.DELETED:
-      return rows.filter((row) => row.deleted)
+      return table.filter((row) => row.active)
     case TAB_TYPES.ACTIVE:
-      return rows.filter((row) => !row.deleted)
+      return table.filter((row) => !row.active)
     case TAB_TYPES.ALL:
     default:
-      return rows
+      return table
   }
+}
+
+export const getRows = (state) => {
+  const { table } = state
+  return table
 }
